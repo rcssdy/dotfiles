dd7ef91d30542d3609cd325057d3bdf1be63a685
Ryan Cassidy <ryanjamescassidy@protonmail.com>
2026-01-17 22:56:28 +0000

    fix(nvim): improve TypeScript LSP and Treesitter config
    - Rename tsserver to ts_ls (LazyVim convention)
    - Add eslint-lsp and prettierd to Mason
    - Add missing Treesitter parsers: typescript, tsx, javascript,
      json, jsonc, html, lua, bash, vim, vimdoc, markdown, yaml, regex

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -12,6 +12,8 @@ return {
         "tailwindcss-language-server",
         "typescript-language-server",
         "css-lsp",
+        "eslint-lsp",
+        "prettierd",
       })
     end,
   },
@@ -28,7 +30,7 @@ return {
             return require("lspconfig.util").root_pattern(".git")(...)
           end,
         },
-        tsserver = {
+        ts_ls = {
           root_dir = function(...)
             return require("lspconfig.util").root_pattern(".git")(...)
           end,
