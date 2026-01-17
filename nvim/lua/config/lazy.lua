1744410a9ac9725dce1d7aeb49b594377c759102
Ryan Cassidy <ryanjamescassidy@protonmail.com>
2026-01-17 15:24:41 +0000

    chore(nvim): add Python and TypeScript extras, update plugins
    - Add lazyvim.plugins.extras.lang.python and lang.typescript
    - Add venv-selector.nvim for Python virtualenv selection
    - Remove cursor-dark.nvim plugin
    - Move octo extra to lazyvim.json for proper LazyVim format

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -18,8 +18,6 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- LazyVim extras
-    { import = "lazyvim.plugins.extras.util.octo" },
     -- import/override with your plugins
     { import = "plugins" },
   },
